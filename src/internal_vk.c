cd2dc5a561f9184897029a7445a4f1c3fb4de354
Voldie <voldiekami@gmail.com>
2016-10-29 10:53:17 +0200

    Started implementing vulkan.

--- src/internal_vk.c
+++ src/internal_vk.c
@@ -1,14 +1,91 @@
 #include"internal.h"
 #include<vulkan/vulkan.h>
 
+VkInstance instance;
+VkPhysicalDevice kdevice;
+VkDevice device;
+VkQueue queue;
+VkCommandBuffer commandbuffer;
+
+
+
+int glslview_init_vulkan(ExWin window){
+	VkResult result;
+	unsigned int num_devices;
+	uint32_t queue_families_count = 0;
+	VkPhysicalDevice stackdevice[8];
+	VkApplicationInfo appInfo = {};
+
+	appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+	appInfo.pApplicationName = "glslview";
+	appInfo.applicationVersion = VK_MAKE_VERSION(0,6,5);
+	appInfo.pEngineName = "";
+	appInfo.engineVersion = VK_MAKE_VERSION(1,0,0);
+	appInfo.apiVersion = VK_API_VERSION_1_0;
+
+
+	VkInstanceCreateInfo createinfo = {};
+	createinfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+	createinfo.pApplicationInfo = &appInfo;
+
+	result = vkCreateInstance(&createinfo, NULL, &instance);
+	if(result != VK_SUCCESS){
+
+	}
+
+
+	if( vkEnumeratePhysicalDevices( instance, &num_devices, NULL ) != VK_SUCCESS){
+
+	}
+
+
+
+	if( vkEnumeratePhysicalDevices( instance, &num_devices, &stackdevice[0] ) != VK_SUCCESS ) {
+
+	}
+
+
+	vkGetPhysicalDeviceQueueFamilyProperties( kdevice, &queue_families_count, NULL );
+
+
+	VkDeviceQueueCreateInfo queueinfo = {};
+	queueinfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+
+	VkDeviceCreateInfo deviceinfo = {};
+	deviceinfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+
+
+	result = vkCreateDevice(kdevice, &deviceinfo, NULL, &device);
+
+
+
+
+
+
+	VkDebugReportCallbackCreateInfoEXT createInfo = {};
+	createInfo.sType = VK_STRUCTURE_TYPE_DEBUG_REPORT_CALLBACK_CREATE_INFO_EXT;
+	createInfo.flags = VK_DEBUG_REPORT_ERROR_BIT_EXT | VK_DEBUG_REPORT_WARNING_BIT_EXT;
+	/*createInfo.pfnCallback = debugCallback;*/
+
+
+}
 
 void glslview_resize_screen_vk(ExEvent* event, struct uniform_location_t* uniform, ExShader* shader, ExTexture* ftexture){
+	VkViewport viewport;
+	viewport.x = 0;
+	viewport.y = 0;
+	viewport.width = event->size.width;
+	viewport.height = event->size.height;
+	viewport.minDepth = 1.0;
+	viewport.maxDepth = 0.0;
 
+	vkCmdSetViewport(commandbuffer, 1, 1, &viewport);
 
 }
 
 
 void glslview_update_shader_uniform_vk(struct uniform_location_t* uniform, ExShader* shader, int width, int height){
+
 
 }
 
