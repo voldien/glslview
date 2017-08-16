b394379ffc4824ed698e8da234299dba953cf6f3
voldie <voldiekami@gmail.com>
2017-08-16 16:15:10 +0200

    Code cleanup.

--- include/rendering.h
+++ include/rendering.h
@@ -1,7 +1,25 @@
-#ifndef ___H_
-#define ___H_ 1
-#include"def.h"
+/**
+    glslview
+    Copyright (C) 2017  Valdemar Lindberg
 
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+#ifndef _GLSLVIEW_RENDERING_H_
+#define _GLSLVIEW_RENDERING_H_ 1
+#include"glslview.h"
+#include<SDL2/SDL.h>
 
 
 /**
@@ -18,7 +36,8 @@ extern SDL_Window* glslview_gl_init(void);
 extern void glslview_gl_release(void);
 
 /**
- *	TODO add enum for texture target.
+ *	Create texture. Uses similar function paramter call as
+ *	glTexImage2D.
  *
  *	@Return
  */
@@ -44,7 +63,7 @@ extern void glslview_gl_resize_screen(int* __restrict__ res,
 		glslviewTexture* __restrict__ ftexture);
 
 /**
- *
+ *	Update shader uniforms.
  */
 extern void glslview_gl_update_shader_uniform(
 		UniformLocation* __restrict__ uniform,
